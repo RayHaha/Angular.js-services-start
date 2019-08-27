d09f398c46ce642c1f2566627632ec3b949e2b29
RayHaha <g820324@gmail.com>
2019-08-26 22:15:37 -0400

    new account component

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -13,10 +13,10 @@ export class NewAccountComponent {
   // @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
   constructor(//private loggingService: LoggingService,
-              private accountsService: AccountsService){
-                this.accountsService.statusUpdated.subscribe(
-                  (status:string) => alert('New status: ' + status)
-                );
+    private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New status: ' + status)
+    );
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
